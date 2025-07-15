a1f4d5b63522c34b7e7d75550ace4eec99396a47
Bobby Sills <backsideofdawn@gmail.com>
2025-07-14 21:02:40 -0700

    deleted a file

--- lib/exercises/learn_vowel_exercise.dart
+++ lib/exercises/learn_vowel_exercise.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:learn_lao_app/components/bottom_lesson_button.dart';
+import 'package:learn_lao_app/components/dynamic_bold_text.dart';
 import 'package:learn_lao_app/exercises/stateful_exercise.dart';
+import 'package:learn_lao_app/utilities/helper_functions.dart';
 import 'package:learn_lao_app/utilities/provider/lesson_provider.dart';
 import 'package:learn_lao_app/utilities/shared_styles.dart';
 import 'package:learn_lao_app/utilities/sounds_utility.dart';
@@ -9,12 +11,12 @@ import 'package:provider/provider.dart';
 class LearnVowelExercise extends StatefulExercise {
   final String letter;
   final int letterIndex;
-  final String placeholder;
+  final String? placeholder;
 
   LearnVowelExercise({
     required this.letter,
     required this.letterIndex,
-    this.placeholder = "",
+    this.placeholder,
     super.key,
   });
 
@@ -26,6 +28,12 @@ class LearnVowelExerciseState
     extends StatefulExerciseState<LearnVowelExercise> {
   final _speechPlayer = SoundsUtility();
   final _effectPlayer = SoundsUtility();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget bottomSheetContent(BuildContext context) {
     return Column(
@@ -51,12 +59,27 @@ class LearnVowelExerciseState
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+    final bool isDarkMode = theme.brightness == Brightness.dark;
 
     return Expanded(
       child: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            Expanded(
+              flex: 2,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: DynamicBoldText(
+                  text: widget.letter,
+                  targetCharacter: vowelPlaceholder,
+                  fontSize: 200,
+                  boldColor: isDarkMode ? Colors.white : Colors.black,
+                  outlineColor: isDarkMode ? Colors.white : Colors.black,
+                  strokeWidth: 2.5,
+                ),
+              ),
+            ),
             Expanded(
               flex: 5,
               child: FittedBox(
