d8f20bb2840a0fc87c468eb13b4b1da30a88382e
Bobby Sills <backsideofdawn@gmail.com>
2025-08-10 13:46:16 +0700

    Aug 10 2025

--- lib/exercises/arrange_letters/types.dart
+++
@@ -1,8 +0,0 @@
-enum Section { upper, lower }
-
-typedef DropLocation = (int, Section);
-
-extension CopyableDropLocation on DropLocation {
-  DropLocation copyWith({int? index, Section? section}) =>
-      (index ?? this.$1, section ?? this.$2);
-}
--- lib/lesson_generators/consonant_lesson_generator.dart
+++ lib/lesson_generators/consonant_lesson_generator.dart
@@ -7,285 +7,101 @@ import 'package:learn_lao_app/exercises/select_letter_exercise.dart';
 import 'package:learn_lao_app/exercises/stateful_exercise.dart';
 import 'package:learn_lao_app/utilities/helper_functions.dart';
 import 'package:learn_lao_app/utilities/letter_data.dart';
+import 'package:collection/collection.dart';
 
 class ConsonantLessonGenerator {
-  // Complete 3-lesson cycle generator for Lao consonant learning
-  // Input: newLetters (3 new consonants), oldLetters (3 previously learned)
-
-  List<List<StatefulExercise>> generateLessonCycle(
-    List<String> newLetters,
-    List<String> oldLetters,
-  ) {
-    List<List<StatefulExercise>> generatedLessons = [[], [], []];
-
-    // LESSON 1: Introduction & Initial Practice
-    // ========================================
-
-    // 1. New Letter Introduction
-    for (var letter in newLetters) {
-      generatedLessons[0].addAll([
-        LearnConsonantExercise(
-          letter: letter,
-          word: LetterData.lettersToWords[letter]!,
-        ),
-        SelectLetterExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(newLetters, 3, letter),
-          sectionType: SectionType.consonant,
-        ),
-        SelectSoundExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(newLetters, 3, letter),
-          sectionType: SectionType.consonant,
-        ),
+  List<StatefulExercise> generateLesson(List<String> letters) {
+    final List<StatefulExercise> lessons = [];
+    for (String letter in letters) {
+      lessons.addAll([
+        LearnConsonantExercise(letter: letter),
+        ...[
+          SelectSoundExercise(
+            correctLetter: letter,
+            incorrectLetters: pickCountExcluding(
+              letters,
+              (letters.length - 1).clamp(0, 2),
+              letter,
+            ),
+            sectionType: SectionType.consonant,
+          ),
+          SelectLetterExercise(
+            correctLetter: letter,
+            incorrectLetters: pickCountExcluding(
+              letters,
+              (letters.length - 1).clamp(0, 2),
+              letter,
+            ),
+            sectionType: SectionType.consonant,
+          ),
+        ]..shuffle(),
       ]);
     }
 
-    // 2. Old Letter Review
-    generatedLessons[0].add(
-      MatchingExercise(
-        lettersToMatch: oldLetters,
-        sectionType: SectionType.consonant,
-      ),
-    );
+    // If are there are more than 5 letters to match, split them up into two different sets of matching exercises
+    final Iterable<List<String>> listOfMatches = (letters.length > 5)
+        ? letters.slices(3)
+        : [letters];
 
-    // 3. Mixed Practice (randomized)
-    List<String> combinedLetters = <String>{
-      ...oldLetters,
-      ...newLetters,
-    }.toList();
-    combinedLetters.shuffle();
+    // Loop over each set of letters to matche
+    for (List<String> matches in listOfMatches) {
+      lessons.add(
+        MatchingExercise(
+          lettersToMatch: matches,
+          sectionType: SectionType.consonant,
+        ),
+      );
+    }
 
-    final shuffledExercises = <StatefulExercise>[];
-    for (var letter in combinedLetters) {
+
+    final List<StatefulExercise> shuffledExercises = [];
+
+    for (String letter in letters) {
       shuffledExercises.addAll([
-        SelectSoundExercise(
-          correctLetter: letter,
-          incorrectLetters: pickCountIncluding(combinedLetters, 3, letter),
-          sectionType: SectionType.consonant,
-        ),
-        SelectLetterExercise(
-          correctLetter: letter,
-          incorrectLetters: pickCountIncluding(combinedLetters, 3, letter),
-          sectionType: SectionType.consonant,
-        ),
-      ]);
-    }
-    shuffledExercises.shuffle();
-    generatedLessons[0].addAll(shuffledExercises);
+          SelectSoundExercise(
+            correctLetter: letter,
+            incorrectLetters: pickCountExcluding(
+              letters,
+              (letters.length - 1).clamp(0, 2),
+              letter,
+            ),
+            sectionType: SectionType.consonant,
+          ),
+          SelectLetterExercise(
+            correctLetter: letter,
+            incorrectLetters: pickCountExcluding(
+              letters,
+              (letters.length - 1).clamp(0, 2),
+              letter,
+            ),
+            sectionType: SectionType.consonant,
+          )]);
+      }
+    lessons.addAll(
+    [
+    ...letters.map((letter)=> ...[SelectSoundExercise(
+            correctLetter: letter,
+            incorrectLetters: pickCountExcluding(
+              letters,
+              (letters.length - 1).clamp(0, 2),
+              letter,
+            ),
+            sectionType: SectionType.consonant,
+          ),
+          SelectLetterExercise(
+            correctLetter: letter,
+            incorrectLetters: pickCountExcluding(
+              letters,
+              (letters.length - 1).clamp(0, 2),
+              letter,
+            ),
+            sectionType: SectionType.consonant,
+          )]))
 
-    // 4. Final Assessment - Split into groups of max 5 letters
-    List<String> finalShuffled = [...combinedLetters];
-    finalShuffled.shuffle();
-    generatedLessons[0].addAll([
-      MatchingExercise(
-        lettersToMatch: finalShuffled.sublist(0, 3),
-        sectionType: SectionType.consonant,
-      ),
-      MatchingExercise(
-        lettersToMatch: finalShuffled.sublist(3, 6),
-        sectionType: SectionType.consonant,
-      ),
-    ]);
+      for (String letter in letters)
+          ...    ]..shuffle());
 
-    // LESSON 2: Reinforcement & Integration
-    // ====================================
-
-    // 1. Quick Review (new letters only)
-    for (var letter in newLetters) {
-      generatedLessons[1].add(
-        SelectLetterExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(newLetters, 3, letter),
-          sectionType: SectionType.consonant,
-        ),
-      );
-    }
-
-    // 2. Focused New Letter Practice
-    generatedLessons[1].add(
-      MatchingExercise(
-        lettersToMatch: newLetters,
-        sectionType: SectionType.consonant,
-      ),
-    );
-
-    for (var letter in newLetters) {
-      generatedLessons[1].add(
-        SelectSoundExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(
-            combinedLetters,
-            4,
-            letter,
-          ), // Harder: 4 options
-          sectionType: SectionType.consonant,
-        ),
-      );
-    }
-
-    // 3. Integration Challenges
-    List<String> randomSubset1 = getRandomSubset(combinedLetters, 4);
-    List<String> randomSubset2 = getRandomSubset(combinedLetters, 4);
-
-    for (var letter in randomSubset1) {
-      generatedLessons[1].add(
-        SelectLetterExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-      );
-    }
-
-    for (var letter in randomSubset2) {
-      generatedLessons[1].add(
-        SelectSoundExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-      );
-    }
-
-    // 4. Comprehensive Review
-    // Split combined letters into groups of max 5 for matching
-    List<String> shuffledCombined = [...combinedLetters];
-    shuffledCombined.shuffle();
-    generatedLessons[1].add(
-      MatchingExercise(
-        lettersToMatch: shuffledCombined.take(5).toList(),
-        sectionType: SectionType.consonant,
-      ),
-    );
-    List<String> finalMixed = [...combinedLetters];
-    finalMixed.shuffle();
-    for (var letter in finalMixed) {
-      generatedLessons[1].add(
-        Random().nextBool()
-            ? SelectLetterExercise(
-                correctLetter: letter,
-                allLetters: pickCountIncluding(combinedLetters, 3, letter),
-                sectionType: SectionType.consonant,
-              )
-            : SelectSoundExercise(
-                correctLetter: letter,
-                allLetters: pickCountIncluding(combinedLetters, 3, letter),
-                sectionType: SectionType.consonant,
-              ),
-      );
-    }
-
-    // LESSON 3: Mastery & Preparation
-    // ===============================
-
-    // 1. Mastery Check (new letters)
-    for (var letter in newLetters) {
-      generatedLessons[2].addAll([
-        SelectSoundExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(
-            combinedLetters,
-            4,
-            letter,
-          ), // Max difficulty
-          sectionType: SectionType.consonant,
-        ),
-        SelectLetterExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-      ]);
-    }
-
-    // 2. Speed Challenges
-    List<String> speedRound1 = getRandomSubset(combinedLetters, 6);
-    List<String> speedRound2 = getRandomSubset(combinedLetters, 6);
-
-    for (var letter in speedRound1) {
-      generatedLessons[2].add(
-        SelectLetterExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-      );
-    }
-
-    for (var letter in speedRound2) {
-      generatedLessons[2].add(
-        SelectSoundExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-      );
-    }
-
-    // 3. Complex Matching - Split combined letters into groups of max 5
-    List<String> matchingShuffled = [...combinedLetters];
-    matchingShuffled.shuffle();
-    generatedLessons[2].add(
-      MatchingExercise(
-        lettersToMatch: matchingShuffled.take(5).toList(),
-        sectionType: SectionType.consonant,
-      ),
-    );
-
-    // Split matching with mixed old/new
-    List<String> splitGroup1 = [oldLetters[0], oldLetters[1], newLetters[0]];
-    List<String> splitGroup2 = [oldLetters[2], newLetters[1], newLetters[2]];
-
-    generatedLessons[2].addAll([
-      MatchingExercise(
-        lettersToMatch: splitGroup1,
-        sectionType: SectionType.consonant,
-      ),
-      MatchingExercise(
-        lettersToMatch: splitGroup2,
-        sectionType: SectionType.consonant,
-      ),
-    ]);
-
-    // 4. Final Assessment
-    List<String> comprehensiveMixed = [...combinedLetters];
-    comprehensiveMixed.shuffle();
-
-    for (var letter in comprehensiveMixed) {
-      // Random exercise type for variety
-      var exerciseTypes = [
-        () => SelectLetterExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-        () => SelectSoundExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-      ];
-      generatedLessons[2].add(exerciseTypes[Random().nextInt(2)]());
-    }
-
-    // Graduation check: rapid-fire new letters
-    for (var letter in newLetters) {
-      generatedLessons[2].addAll([
-        SelectLetterExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-        SelectSoundExercise(
-          correctLetter: letter,
-          allLetters: pickCountIncluding(combinedLetters, 4, letter),
-          sectionType: SectionType.consonant,
-        ),
-      ]);
-    }
-
-    return generatedLessons;
+    return lessons;
   }
 
   // Helper function to get a random subset of specified size
@@ -317,12 +133,12 @@ class ConsonantLessonGenerator {
         ),
         SelectLetterExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
         SelectSoundExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       ]);
@@ -344,12 +160,12 @@ class ConsonantLessonGenerator {
       generatedLessons[0].addAll([
         SelectSoundExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
         SelectLetterExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       ]);
@@ -371,7 +187,7 @@ class ConsonantLessonGenerator {
       generatedLessons[1].add(
         SelectLetterExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       );
@@ -390,7 +206,7 @@ class ConsonantLessonGenerator {
       generatedLessons[1].add(
         SelectSoundExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       );
@@ -404,12 +220,20 @@ class ConsonantLessonGenerator {
         Random().nextBool()
             ? SelectLetterExercise(
                 correctLetter: letter,
-                allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+                incorrectLetters: pickCountExcluding(
+                  firstThreeLetters,
+                  2,
+                  letter,
+                ),
                 sectionType: SectionType.consonant,
               )
             : SelectSoundExercise(
                 correctLetter: letter,
-                allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+                incorrectLetters: pickCountExcluding(
+                  firstThreeLetters,
+                  2,
+                  letter,
+                ),
                 sectionType: SectionType.consonant,
               ),
       );
@@ -423,12 +247,12 @@ class ConsonantLessonGenerator {
       generatedLessons[2].addAll([
         SelectSoundExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
         SelectLetterExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       ]);
@@ -452,7 +276,7 @@ class ConsonantLessonGenerator {
       generatedLessons[2].add(
         SelectLetterExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       );
@@ -462,7 +286,7 @@ class ConsonantLessonGenerator {
       generatedLessons[2].add(
         SelectSoundExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       );
@@ -475,12 +299,12 @@ class ConsonantLessonGenerator {
       generatedLessons[2].addAll([
         SelectLetterExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
         SelectSoundExercise(
           correctLetter: letter,
-          allLetters: pickCountIncluding(firstThreeLetters, 3, letter),
+          incorrectLetters: pickCountExcluding(firstThreeLetters, 2, letter),
           sectionType: SectionType.consonant,
         ),
       ]);
@@ -495,47 +319,52 @@ class ConsonantLessonGenerator {
   /// Generates the complete curriculum for all letters as a flat list
   /// Returns a single list of all lessons in order
   List<List<StatefulExercise>> generateCompleteCurriculum(
-    List<String> allLetters,
+    List<String> incorrectLetters,
   ) {
     List<List<StatefulExercise>> allLessons = [];
 
     // Ensure we have at least 3 letters to work with
-    if (allLetters.length < 3) {
+    if (incorrectLetters.length < 3) {
       throw ArgumentError('Need at least 3 letters to generate lessons');
     }
 
     // First cycle: Use special first lesson logic to avoid duplicate letter issues
-    List<String> firstThree = allLetters.take(3).toList();
+    List<String> firstThree = incorrectLetters.take(3).toList();
     List<List<StatefulExercise>> firstCycle = generateFirstLessonCycle(
       firstThree,
     );
     allLessons.addAll(firstCycle);
 
     // Subsequent cycles: 3 new letters + 3 from previous cycle
-    for (int i = 3; i < allLetters.length; i += 3) {
+    for (int i = 3; i < incorrectLetters.length; i += 3) {
       List<String> newLetters = [];
-      List<String> oldLetters = allLetters.sublist(
+      List<String> oldLetters = incorrectLetters.sublist(
         i - 3,
         i,
       ); // Previous 3 letters
 
       // Get the next 3 letters (or remaining letters if less than 3)
-      int endIndex = (i + 3 <= allLetters.length) ? i + 3 : allLetters.length;
-      newLetters = allLetters.sublist(i, endIndex);
+      int endIndex = (i + 3 <= incorrectLetters.length)
+          ? i + 3
+          : incorrectLetters.length;
+      newLetters = incorrectLetters.sublist(i, endIndex);
 
       // If we have fewer than 3 new letters, pad with recent letters for practice
-      while (newLetters.length < 3 && allLetters.length >= 3) {
+      while (newLetters.length < 3 && incorrectLetters.length >= 3) {
         // Add letters from earlier in the curriculum for review
-        int paddingIndex = (i - 6 + newLetters.length) % allLetters.length;
+        int paddingIndex =
+            (i - 6 + newLetters.length) % incorrectLetters.length;
         if (paddingIndex < 0) paddingIndex = newLetters.length;
 
-        String paddingLetter = allLetters[paddingIndex];
+        String paddingLetter = incorrectLetters[paddingIndex];
         if (!newLetters.contains(paddingLetter) &&
             !oldLetters.contains(paddingLetter)) {
           newLetters.add(paddingLetter);
         } else {
           // If we can't find a unique letter, just repeat from the start
-          newLetters.add(allLetters[newLetters.length % allLetters.length]);
+          newLetters.add(
+            incorrectLetters[newLetters.length % incorrectLetters.length],
+          );
         }
       }
 
